528d3a34601bdb28e1af49302e73f3e2da4272cf
Cyclops-THSS <jlmhkwang@gmail.com>
2016-06-28 11:49:11 +0800

    sorry again..
    mistyped..

--- user.h
+++ user.h
@@ -1,9 +1,9 @@
-#define CONS_NO_BUFFER  0x1
-#define CONS_BUFFER    0x0
 #define CONS_CGREEN     0xa0
 #define CONS_CRED       0xc0
 #define CONS_CBLUE      0x90
 #define CONS_CDEFAULT   0x70
+#define CONS_NO_BUFFER  0x1 | CONS_CDEFAULT
+#define CONS_BUFFER    	0x0 | CONS_CDEFAULT
 
 struct stat;
 struct rtcdate;
